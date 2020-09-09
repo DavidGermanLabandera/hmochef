01128b1dd1693d60a3d107c853bb7e210d5ced05
David Germán <dgerman@teknol.com.mx>
2020-09-08 21:40:02 -0700

    cambio coneccion

--- modo-produccion/frontend/modelos/conexion.php
+++ modo-produccion/frontend/modelos/conexion.php
@@ -4,9 +4,9 @@ class Conexion{
 
 	static public function conectar(){
 
-		$link = new PDO("mysql:host=localhost;dbname=ferrete5_ecommerce",
-						"ferrete5_juanu",
-						"******",
+		$link = new PDO("mysql:host=localhost;dbname=hmochef",
+						"root",
+						"12345678",
 						array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
 		                      PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
 						);
